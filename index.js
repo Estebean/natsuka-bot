adf3878df8bfdebaca578b31c4ded9f7f3966f09
Estebean <46071471+Estebean@users.noreply.github.com>
2018-12-22 17:21:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const prefix = ("?");
 
 bot.on('ready', function () {
     console.log("Je suis prêt à être utilisé.")
-    bot.user.setActivity('rien').catch(console.error)
+    bot.user.setActivity('manger des chips !').catch(console.error)
 });
 
 bot.on('guildMemberAdd', member => {
